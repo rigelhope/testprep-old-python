b11c929e9f1fbaa918ee87ccf71ecff28e33d22d
rigel hope <testprep@rigelhope.org>
2013-09-24 21:33:39 -0700

    main python file name change oops

---
+++ testprep.py
@@ -0,0 +1,36 @@
+import cherrypy
+from lxml import etree
+from lxml.builder import E
+import os.path
+
+class Root(object):
+
+    def __init__(self):
+        self._xmlsource = etree.parse(open('testdata.xml', 'rb'))
+        self._xmlroot = self._xmlsource.getroot()
+        self._transform = etree.XSLT(etree.parse(open('testview.xsl', 'rb')))
+        self.html = self._transform(self._xmlsource)
+        
+
+    @cherrypy.expose
+    def index(self):
+        return str(self.html)
+
+    @cherrypy.expose
+    def send(self, **params):
+        print params
+        h = E.history(str(params))
+        self._xmlroot.append(h)
+        self._out = open('testhistory.xml', 'wb')
+        self._out.write(etree.tostring(self._xmlroot))
+        self._out.close()               
+
+if __name__ == '__main__':
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    conf = {'/static': {'tools.staticdir.on': True,
+                        'tools.staticdir.dir': os.path.join(current_dir, 'static')
+                        }}
+
+
+cherrypy.quickstart(Root(), '', config=conf)
